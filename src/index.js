3d5cc22cef5515f52732632b1523a769b6ac3f8c
KatoJun <jun.kato0208@gmail.com>
2021-10-21 15:10:58 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button（完了）生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 完了リストに追加
+
+    // div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+    // li生成
+    const li = document.createElement("li");
+    li.innerText = completeButton.parentNode.getElementsByTagName(
+      "li"
+    )[0].innerText;
+
+    // button（戻す）生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {});
+
+    div.appendChild(li);
+    div.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(div);
+
+    // 未完了リストから削除
+  });
+
+  // button（削除）生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストを削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // divの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
